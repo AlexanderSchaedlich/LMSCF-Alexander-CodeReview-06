8c8b4513373982bdddc24d4223643647a05c945a
Alexander Schaedlich <alexander.schaedlich@gmail.com>
2020-02-24 23:26:43 +0100

    bonus points

--- js/script.js
+++ js/script.js
@@ -14,35 +14,34 @@ var __extends = (this && this.__extends) || (function () {
 var myArray = [];
 var myPosts = $(".myPosts");
 var MyPlace = /** @class */ (function () {
-    function MyPlace(name, adress, zipCode, city, img, date, time) {
+    function MyPlace(name, adress, zipCode, city, img, dateAndTime) {
         this.name = "";
         this.adress = "";
         this.zipCode = 0;
         this.city = "";
         this.img = "";
-        this.date = "";
-        this.time = "";
+        this.dateAndTime = "";
         this.name = name;
         this.adress = adress;
         this.zipCode = zipCode;
         this.city = city;
         this.img = img;
-        this.date = date;
-        this.time = time;
+        this.dateAndTime = dateAndTime;
+        dateAndTime;
     }
     MyPlace.prototype.display = function () {
-        myPosts.append("\n\t\t\t<div class=\"col-sm-12 col-md-6 col-lg-3\">\n\t\t\t\t<div class=\"rounded bg-white p-2 m-1\">\n\t\t\t\t\t<h4 class=\"bg-secondary mb-0\">" + this.name + "</h2>\n\t\t\t\t\t<div class=\"position-relative\">\n\t\t\t\t\t\t<img class=\"d-sm-none d-md-inline\" src=\"" + this.img + "\" style=\"width: 100%; height: auto;\">\n\t\t\t\t\t\t<p class=\"position-absolute text-white m-0\" style=\"bottom: 0; right: 0;\">Created: " + this.date + ", " + this.time + "</p>\n\t\t\t\t\t</div>\n\t\t\t\t\t<p class=\"m-0\">" + this.adress + ", " + this.zipCode + " " + this.city + "</p>\n\t\t\t\t</div>\n\t\t\t</div>\n\t\t");
+        myPosts.append("\n\t\t\t<div class=\"col-sm-12 col-md-6 col-lg-3\">\n\t\t\t\t<div class=\"rounded bg-white p-2 m-1\">\n\t\t\t\t\t<h4 class=\"bg-secondary mb-0\">" + this.name + "</h2>\n\t\t\t\t\t<div class=\"position-relative\">\n\t\t\t\t\t\t<img class=\"d-sm-none d-md-inline\" src=\"" + this.img + "\" style=\"width: 100%; height: auto;\">\n\t\t\t\t\t\t<p class=\"position-absolute text-white m-0\" style=\"bottom: 0; right: 0;\">Created: " + this.dateAndTime + "</p>\n\t\t\t\t\t</div>\n\t\t\t\t\t<p class=\"m-0\">" + this.adress + ", " + this.zipCode + " " + this.city + "</p>\n\t\t\t\t</div>\n\t\t\t</div>\n\t\t");
     };
     return MyPlace;
 }());
-var karlskirche = new MyPlace("St. Charles Church", "Karlsplatz 1", 1010, "Vienna", "img/vienna.jfif", "Jan 01 2020", "00:00");
+var karlskirche = new MyPlace("St. Charles Church", "Karlsplatz 1", 1010, "Vienna", "img/vienna.jfif", "Jan 01 2020 00:00");
 myArray.push(karlskirche);
-var schoenbrunn = new MyPlace("Schönbrunn Park", "Maxingstraße 13b", 1130, "Vienna", "img/schoenbrunn.jfif", "Jan 01 2020", "00:10");
+var schoenbrunn = new MyPlace("Schönbrunn Park", "Maxingstraße 13b", 1130, "Vienna", "img/schoenbrunn.jfif", "Jan 01 2020 00:10");
 myArray.push(schoenbrunn);
 var MyRestaurant = /** @class */ (function (_super) {
     __extends(MyRestaurant, _super);
-    function MyRestaurant(name, adress, zipCode, city, img, date, time, telNumber, type, webAdress) {
-        var _this = _super.call(this, name, adress, zipCode, city, img, date, time) || this;
+    function MyRestaurant(name, adress, zipCode, city, img, dateAndTime, telNumber, type, webAdress) {
+        var _this = _super.call(this, name, adress, zipCode, city, img, dateAndTime) || this;
         _this.telNumber = "";
         _this.type = "";
         _this.webAdress = "";
@@ -52,18 +51,18 @@ var MyRestaurant = /** @class */ (function (_super) {
         return _this;
     }
     MyRestaurant.prototype.display = function () {
-        myPosts.append("\n\t\t\t<div class=\"col-sm-12 col-md-6 col-lg-3\">\n\t\t\t\t<div class=\"rounded bg-white p-2 m-1\">\n\t\t\t\t\t<h4 class=\"bg-secondary mb-0\">" + this.name + "</h2>\n\t\t\t\t\t<div class=\"position-relative\">\n\t\t\t\t\t\t<img class=\"d-sm-none d-md-inline\" src=\"" + this.img + "\" style=\"width: 100%; height: auto;\">\n\t\t\t\t\t\t<p class=\"position-absolute text-white m-0\" style=\"bottom: 0; right: 0;\">Created: " + this.date + ", " + this.time + "</p>\n\t\t\t\t\t</div>\n\t\t\t\t\t<p class=\"m-0\">" + this.adress + ", " + this.zipCode + " " + this.city + ", " + this.telNumber + "</p>\n\t\t\t\t\t<p class=\"m-0\">" + this.type + "</p>\n\t\t\t\t\t<a href=\"" + this.webAdress + "\" target=\"_blank\"><p class=\"m-0\">" + this.webAdress + "</p></a>\n\t\t\t\t</div>\n\t\t\t</div>\n\t\t");
+        myPosts.append("\n\t\t\t<div class=\"col-sm-12 col-md-6 col-lg-3\">\n\t\t\t\t<div class=\"rounded bg-white p-2 m-1\">\n\t\t\t\t\t<h4 class=\"bg-secondary mb-0\">" + this.name + "</h2>\n\t\t\t\t\t<div class=\"position-relative\">\n\t\t\t\t\t\t<img class=\"d-sm-none d-md-inline\" src=\"" + this.img + "\" style=\"width: 100%; height: auto;\">\n\t\t\t\t\t\t<p class=\"position-absolute text-white m-0\" style=\"bottom: 0; right: 0;\">Created: " + this.dateAndTime + "</p>\n\t\t\t\t\t</div>\n\t\t\t\t\t<p class=\"m-0\">" + this.adress + ", " + this.zipCode + " " + this.city + ", " + this.telNumber + "</p>\n\t\t\t\t\t<p class=\"m-0\">" + this.type + "</p>\n\t\t\t\t\t<a href=\"" + this.webAdress + "\" target=\"_blank\"><p class=\"m-0\">" + this.webAdress + "</p></a>\n\t\t\t\t</div>\n\t\t\t</div>\n\t\t");
     };
     return MyRestaurant;
 }(MyPlace));
-var onRestaurant = new MyRestaurant("ON Restaurant", "Wehrgasse 8", 1050, "Vienna", "img/onRestaurant.jfif", "Jan 01 2020", "10:10", "+43(1)5854900", "Chinese", "http://www.restaurant-on.at/");
+var onRestaurant = new MyRestaurant("ON Restaurant", "Wehrgasse 8", 1050, "Vienna", "img/onRestaurant.jfif", "Jan 01 2020 10:10", "+43(1)5854900", "Chinese", "http://www.restaurant-on.at/");
 myArray.push(onRestaurant);
-var bioFrische = new MyRestaurant("BioFrische", "Stutterheimstraße 6", 1150, "Vienna", "img/bioFrische.jfif", "Jan 10 2020", "10:10", "+43(1) 9529215", "Indian", "https://biofrische.wien");
+var bioFrische = new MyRestaurant("BioFrische", "Stutterheimstraße 6", 1150, "Vienna", "img/bioFrische.jfif", "Jan 10 2020 10:10", "+43(1) 9529215", "Indian", "https://biofrische.wien");
 myArray.push(bioFrische);
 var MyEvent = /** @class */ (function (_super) {
     __extends(MyEvent, _super);
-    function MyEvent(name, adress, zipCode, city, img, date, time, webAdress, eventDate, eventTime, ticketPrice) {
-        var _this = _super.call(this, name, adress, zipCode, city, img, date, time) || this;
+    function MyEvent(name, adress, zipCode, city, img, dateAndTime, webAdress, eventDate, eventTime, ticketPrice) {
+        var _this = _super.call(this, name, adress, zipCode, city, img, dateAndTime) || this;
         _this.webAdress = "";
         _this.eventDate = "";
         _this.eventTime = "";
@@ -75,115 +74,109 @@ var MyEvent = /** @class */ (function (_super) {
         return _this;
     }
     MyEvent.prototype.display = function () {
-        myPosts.append("\n\t\t\t<div class=\"col-sm-12 col-md-6 col-lg-3\">\n\t\t\t\t<div class=\"rounded bg-white p-2 m-1\">\n\t\t\t\t<h4 class=\"bg-secondary mb-0\">" + this.name + "</h2>\n\t\t\t\t\t<div class=\"position-relative\">\n\t\t\t\t\t\t<img class=\"d-sm-none d-md-inline\" src=\"" + this.img + "\" style=\"width: 100%; height: auto;\">\n\t\t\t\t\t\t<p class=\"position-absolute text-white m-0\" style=\"bottom: 0; right: 0;\">Created: " + this.date + ", " + this.time + "</p>\n\t\t\t\t\t</div>\n\t\t\t\t\t<p class=\"m-0\">" + this.adress + ", " + this.zipCode + " " + this.city + "</p>\n\t\t\t\t\t<p class=\"m-0\">" + this.eventDate + ", " + this.eventTime + "</p>\n\t\t\t\t\t<p class=\"m-0\">\u20AC " + this.ticketPrice + "</p>\n\t\t\t\t\t<a href=\"" + this.webAdress + "\" target=\"_blank\"><p class=\"m-0\">" + this.webAdress + "</p></a>\n\t\t\t\t</div>\n\t\t\t</div>\n\t\t");
+        myPosts.append("\n\t\t\t<div class=\"col-sm-12 col-md-6 col-lg-3\">\n\t\t\t\t<div class=\"rounded bg-white p-2 m-1\">\n\t\t\t\t<h4 class=\"bg-secondary mb-0\">" + this.name + "</h2>\n\t\t\t\t\t<div class=\"position-relative\">\n\t\t\t\t\t\t<img class=\"d-sm-none d-md-inline\" src=\"" + this.img + "\" style=\"width: 100%; height: auto;\">\n\t\t\t\t\t\t<p class=\"position-absolute text-white m-0\" style=\"bottom: 0; right: 0;\">Created: " + this.dateAndTime + "</p>\n\t\t\t\t\t</div>\n\t\t\t\t\t<p class=\"m-0\">" + this.adress + ", " + this.zipCode + " " + this.city + "</p>\n\t\t\t\t\t<p class=\"m-0\">" + this.eventDate + ", " + this.eventTime + "</p>\n\t\t\t\t\t<p class=\"m-0\">\u20AC " + this.ticketPrice + "</p>\n\t\t\t\t\t<a href=\"" + this.webAdress + "\" target=\"_blank\"><p class=\"m-0\">" + this.webAdress + "</p></a>\n\t\t\t\t</div>\n\t\t\t</div>\n\t\t");
     };
     return MyEvent;
 }(MyPlace));
-var cats = new MyEvent("Cats - the musical", "Ronacher- Seilerstätte 9", 1010, "Vienna", "img/cats.png", "Feb 10 2020", "10:10", "http://catsmusical.at", "Feb 10 2020", "10:10", "120");
+var cats = new MyEvent("Cats - the musical", "Ronacher- Seilerstätte 9", 1010, "Vienna", "img/cats.png", "Feb 10 2020 10:10", "http://catsmusical.at", "Feb 10 2020", "10:10", "120");
 myArray.push(cats);
-var guns = new MyEvent("Guns ‘n Roses", "Ernst-Happel Stadion, Meiereistraße 7", 1020, "Vienna", "img/guns.jfif", "Feb 10 2021", "10:10", "http://catsmusical.at", "Feb 10 2021", "10:10", "95,50");
+var guns = new MyEvent("Guns ‘n Roses", "Ernst-Happel Stadion, Meiereistraße 7", 1020, "Vienna", "img/guns.jfif", "Feb 10 2021 10:10", "http://catsmusical.at", "Feb 10 2021", "10:10", "95,50");
 myArray.push(guns);
-for (var _i = 0, myArray_1 = myArray; _i < myArray_1.length; _i++) {
-    var value = myArray_1[_i];
-    value.display();
-    // console.log(value.date + " " + value.time);
+function displayThem() {
+    for (var _i = 0, myArray_1 = myArray; _i < myArray_1.length; _i++) {
+        var value = myArray_1[_i];
+        value.display();
+    }
 }
-// console.log(myArray)
-// myPosts = $(".ascending");
-// for(let value of myArray){
-// 	let dateArray = [];
-// 	let d = new Date(value.date);
-// 	let year = $(this).year = d.getFullYear();
-// 	let month = $(this).month = d.getMonth();
-// 	let day = $(this).day = d.getDay();
-// 	console.log(month);
-// 	myArray.sort(function(a,b){ return b.month - a.month });
-// 	value.display();
+displayThem();
+myArray.sort(function (a, b) {
+    var dateA = new Date(a.dateAndTime);
+    var dateB = new Date(b.dateAndTime);
+    return dateB - dateA;
+});
+myPosts = $(".ascending");
+displayThem();
+myArray.reverse();
+myPosts = $(".descending");
+displayThem();
+//first attempt to sort using self-composed multi-layer sort function 
+//(I failed to debug it for lack of time, but I think it's beautiful)
+// function giveMeYear(obj){
+// 	let d = new Date(obj.dateAndTime);
+// 	let year = d.getFullYear();
+// 	return year;
 // }
-function giveMeYear(obj) {
-    var d = new Date(obj.date);
-    var year = d.getFullYear();
-    return year;
-}
-function giveMeMonth(obj) {
-    var d = new Date(obj.date);
-    var month = d.getMonth();
-    return month;
-}
-function giveMeDay(obj) {
-    var d = new Date(obj.date);
-    var day = d.getDate();
-    return day;
-}
-function giveMeHour(obj) {
-    var string = obj.date + " " + obj.time;
-    var d = new Date(string);
-    var hour = d.getHours();
-    return hour;
-}
-function giveMeMinute(obj) {
-    var string = obj.date + " " + obj.time;
-    var d = new Date(string);
-    var minute = d.getMinutes();
-    return minute;
-}
-var layer = 1;
-function myFunction(obj) {
-    var result;
-    switch (true) {
-        case layer == 1:
-            result = giveMeYear(obj);
-            break;
-        case layer == 2:
-            result = giveMeMonth(obj);
-            break;
-        case layer == 3:
-            result = giveMeDay(obj);
-            break;
-        case layer == 4:
-            result = giveMeHour(obj);
-            break;
-        case layer == 5:
-            result = giveMeMinute(obj);
-            break;
-    }
-    return result;
-}
-var itemToCompare;
-var itemToCompareWith;
-function compareItems() {
-    if (myFunction(itemToCompare) < myFunction(itemToCompareWith)) {
-        //swap items
-        var rememberItem = itemToCompare;
-        itemToCompare = itemToCompareWith;
-        itemToCompareWith = rememberItem;
-    }
-    if (myFunction(itemToCompare) == myFunction(itemToCompareWith)) {
-        //go to different layer
-        layer++;
-        compareItems();
-        layer--;
-    }
-}
-for (var i = 0; i < myArray.length; i++) {
-    // console.log(myArray[i]);
-    itemToCompare = myArray[i];
-    for (var moveIndex = 1; moveIndex < myArray.length; moveIndex++) {
-        var newIndex = void 0;
-        if (i + moveIndex < myArray.length) {
-            newIndex = i + moveIndex;
-        }
-        if (i + moveIndex >= myArray.length) {
-            newIndex = i + moveIndex - myArray.length;
-        }
-        itemToCompareWith = myArray[newIndex];
-        compareItems();
-        //write items into array
-        myArray[i] = itemToCompare;
-        myArray[newIndex] = itemToCompareWith;
-    }
-}
-// for(let value of myArray){
-// 	console.log(value.date + " " + value.time);
+// function giveMeMonth(obj){
+// 	let d = new Date(obj.dateAndTime);
+// 	let month = d.getMonth();
+// 	return month;
 // }
-// console.log(myArray)
+// function giveMeDay(obj){
+// 	let d = new Date(obj.dateAndTime);
+// 	let day = d.getDate();
+// 	return day;
+// }
+// function giveMeHour(obj){
+// 	let d = new Date(obj.dateAndTime);
+// 	let hour = d.getHours();
+// 	return hour;
+// }
+// function giveMeMinute(obj){
+// 	let d = new Date(obj.dateAndTime);
+// 	let minute = d.getMinutes();
+// 	return minute;
+// }
+// let layer = 1;
+// function myFunction(obj){
+// 	let result;
+// 	switch(true){
+// 		case layer == 1:
+// 		result = giveMeYear(obj);
+// 		break;
+// 		case layer == 2:
+// 		result = giveMeMonth(obj);
+// 		break;
+// 		case layer == 3:
+// 		result = giveMeDay(obj);
+// 		break;
+// 		case layer == 4:
+// 		result = giveMeHour(obj);
+// 		break;
+// 		case layer == 5:
+// 		result = giveMeMinute(obj);
+// 		break;
+// 	}
+// 	return result;
+// }
+// let itemToCompare;
+// let itemToCompareWith;
+// function compareItems(){
+// 	if(myFunction(itemToCompare) < myFunction(itemToCompareWith)){
+// 		//swap items
+// 		let rememberItem = itemToCompare;
+// 		itemToCompare = itemToCompareWith;
+// 		itemToCompareWith = rememberItem;
+// 	} if(myFunction(itemToCompare) == myFunction(itemToCompareWith)){
+// 		//go to next layer
+// 		layer++;
+// 		compareItems();
+// 		//go back to previous layer
+// 		layer--;
+// 	}
+// }
+// for(let i = 0; i < myArray.length; i++){
+// 	itemToCompare = myArray[i];
+// 	for(let moveIndex = 1; moveIndex < myArray.length; moveIndex++){
+// 		let newIndex;
+// 		if(i + moveIndex < myArray.length){
+// 			newIndex = i + moveIndex;
+// 		} if(i + moveIndex >= myArray.length){
+// 			newIndex = i + moveIndex - myArray.length;
+// 		} 
+// 		itemToCompareWith = myArray[newIndex];
+// 		compareItems();
+// 		//write items into array
+// 		myArray[i] = itemToCompare;
+// 		myArray[newIndex] = itemToCompareWith;
+// 	}
+// }
--- js/script.ts
+++ js/script.ts
@@ -6,16 +6,14 @@ class MyPlace{
 	zipCode = 0;
 	city = "";
 	img = "";
-	date = "";
-	time = "";
-	constructor(name:string, adress:string, zipCode:number, city:string, img:string, date:string, time:string){
+	dateAndTime = "";
+	constructor(name:string, adress:string, zipCode:number, city:string, img:string, dateAndTime:string){
 		this.name = name;
 		this.adress = adress;
 		this.zipCode = zipCode;
 		this.city = city;
 		this.img = img;
-		this.date = date;
-		this.time = time;
+		this.dateAndTime = dateAndTime;dateAndTime
 	}
 	display(){
 		myPosts.append(`
@@ -24,7 +22,7 @@ class MyPlace{
 					<h4 class="bg-secondary mb-0">${this.name}</h2>
 					<div class="position-relative">
 						<img class="d-sm-none d-md-inline" src="${this.img}" style="width: 100%; height: auto;">
-						<p class="position-absolute text-white m-0" style="bottom: 0; right: 0;">Created: ${this.date}, ${this.time}</p>
+						<p class="position-absolute text-white m-0" style="bottom: 0; right: 0;">Created: ${this.dateAndTime}</p>
 					</div>
 					<p class="m-0">${this.adress}, ${this.zipCode} ${this.city}</p>
 				</div>
@@ -32,16 +30,16 @@ class MyPlace{
 		`)
 	}
 }
-let karlskirche = new MyPlace("St. Charles Church", "Karlsplatz 1", 1010, "Vienna", "img/vienna.jfif", "Jan 01 2020", "00:00");
+let karlskirche = new MyPlace("St. Charles Church", "Karlsplatz 1", 1010, "Vienna", "img/vienna.jfif", "Jan 01 2020 00:00");
 myArray.push(karlskirche);
-let schoenbrunn = new MyPlace("Schönbrunn Park", "Maxingstraße 13b", 1130 , "Vienna", "img/schoenbrunn.jfif", "Jan 01 2020", "00:10");
+let schoenbrunn = new MyPlace("Schönbrunn Park", "Maxingstraße 13b", 1130 , "Vienna", "img/schoenbrunn.jfif", "Jan 01 2020 00:10");
 myArray.push(schoenbrunn);
 class MyRestaurant extends MyPlace{
 	telNumber = "";
 	type = "";
 	webAdress = "";
-	constructor(name:string, adress:string, zipCode:number, city:string, img:string, date:string, time:string, telNumber:string, type:string, webAdress:string){
-		super(name, adress, zipCode, city, img, date, time);
+	constructor(name:string, adress:string, zipCode:number, city:string, img:string, dateAndTime:string, telNumber:string, type:string, webAdress:string){
+		super(name, adress, zipCode, city, img, dateAndTime);
 		this.telNumber = telNumber;
 		this.type = type;
 		this.webAdress = webAdress;
@@ -53,7 +51,7 @@ class MyRestaurant extends MyPlace{
 					<h4 class="bg-secondary mb-0">${this.name}</h2>
 					<div class="position-relative">
 						<img class="d-sm-none d-md-inline" src="${this.img}" style="width: 100%; height: auto;">
-						<p class="position-absolute text-white m-0" style="bottom: 0; right: 0;">Created: ${this.date}, ${this.time}</p>
+						<p class="position-absolute text-white m-0" style="bottom: 0; right: 0;">Created: ${this.dateAndTime}</p>
 					</div>
 					<p class="m-0">${this.adress}, ${this.zipCode} ${this.city}, ${this.telNumber}</p>
 					<p class="m-0">${this.type}</p>
@@ -63,17 +61,17 @@ class MyRestaurant extends MyPlace{
 		`)
 	}
 }
-let onRestaurant = new MyRestaurant("ON Restaurant", "Wehrgasse 8", 1050, "Vienna", "img/onRestaurant.jfif", "Jan 01 2020", "10:10", "+43(1)5854900", "Chinese", "http://www.restaurant-on.at/");
+let onRestaurant = new MyRestaurant("ON Restaurant", "Wehrgasse 8", 1050, "Vienna", "img/onRestaurant.jfif", "Jan 01 2020 10:10", "+43(1)5854900", "Chinese", "http://www.restaurant-on.at/");
 myArray.push(onRestaurant);
-let bioFrische = new MyRestaurant("BioFrische", "Stutterheimstraße 6", 1150, "Vienna", "img/bioFrische.jfif", "Jan 10 2020", "10:10", "+43(1) 9529215", "Indian", "https://biofrische.wien");
+let bioFrische = new MyRestaurant("BioFrische", "Stutterheimstraße 6", 1150, "Vienna", "img/bioFrische.jfif", "Jan 10 2020 10:10", "+43(1) 9529215", "Indian", "https://biofrische.wien");
 myArray.push(bioFrische);
 class MyEvent extends MyPlace{
 	webAdress = "";
 	eventDate = "";
 	eventTime = "";
 	ticketPrice = "";
-	constructor(name:string, adress:string, zipCode:number, city:string, img:string, date:string, time:string, webAdress:string, eventDate:string, eventTime:string, ticketPrice:string){
-		super(name, adress, zipCode, city, img, date, time);
+	constructor(name:string, adress:string, zipCode:number, city:string, img:string, dateAndTime:string, webAdress:string, eventDate:string, eventTime:string, ticketPrice:string){
+		super(name, adress, zipCode, city, img, dateAndTime);
 		this.webAdress = webAdress;
 		this.eventDate = eventDate;
 		this.eventTime = eventTime;
@@ -86,7 +84,7 @@ class MyEvent extends MyPlace{
 				<h4 class="bg-secondary mb-0">${this.name}</h2>
 					<div class="position-relative">
 						<img class="d-sm-none d-md-inline" src="${this.img}" style="width: 100%; height: auto;">
-						<p class="position-absolute text-white m-0" style="bottom: 0; right: 0;">Created: ${this.date}, ${this.time}</p>
+						<p class="position-absolute text-white m-0" style="bottom: 0; right: 0;">Created: ${this.dateAndTime}</p>
 					</div>
 					<p class="m-0">${this.adress}, ${this.zipCode} ${this.city}</p>
 					<p class="m-0">${this.eventDate}, ${this.eventTime}</p>
@@ -97,109 +95,104 @@ class MyEvent extends MyPlace{
 		`)
 	}
 }
-let cats = new MyEvent("Cats - the musical", "Ronacher- Seilerstätte 9", 1010, "Vienna", "img/cats.png", "Feb 10 2020", "10:10", "http://catsmusical.at", "Feb 10 2020", "10:10", "120");
+let cats = new MyEvent("Cats - the musical", "Ronacher- Seilerstätte 9", 1010, "Vienna", "img/cats.png", "Feb 10 2020 10:10", "http://catsmusical.at", "Feb 10 2020", "10:10", "120");
 myArray.push(cats);
-let guns = new MyEvent("Guns ‘n Roses", "Ernst-Happel Stadion, Meiereistraße 7", 1020, "Vienna", "img/guns.jfif", "Feb 10 2021", "10:10", "http://catsmusical.at", "Feb 10 2021", "10:10", "95,50");
+let guns = new MyEvent("Guns ‘n Roses", "Ernst-Happel Stadion, Meiereistraße 7", 1020, "Vienna", "img/guns.jfif", "Feb 10 2021 10:10", "http://catsmusical.at", "Feb 10 2021", "10:10", "95,50");
 myArray.push(guns);
-for(let value of myArray){
+function displayThem(){
+	for(let value of myArray){
 	value.display();
-	// console.log(value.date + " " + value.time);
+	}
 }
-// console.log(myArray)
-// myPosts = $(".ascending");
-// for(let value of myArray){
-// 	let dateArray = [];
-// 	let d = new Date(value.date);
-// 	let year = $(this).year = d.getFullYear();
-// 	let month = $(this).month = d.getMonth();
-// 	let day = $(this).day = d.getDay();
-// 	console.log(month);
-	
-// 	myArray.sort(function(a,b){ return b.month - a.month });
-// 	value.display();
+displayThem();
+myArray.sort(function(a, b){
+	let dateA:any = new Date(a.dateAndTime);
+	let dateB:any = new Date(b.dateAndTime);
+	return dateB - dateA;
+})
+myPosts = $(".ascending");
+displayThem();
+myArray.reverse();
+myPosts = $(".descending");
+displayThem();
+//first attempt to sort using self-composed multi-layer sort function 
+//(I failed to debug it for lack of time, but I think it's beautiful)
+// function giveMeYear(obj){
+// 	let d = new Date(obj.dateAndTime);
+// 	let year = d.getFullYear();
+// 	return year;
 // }
-function giveMeYear(obj){
-	let d = new Date(obj.date);
-	let year = d.getFullYear();
-	return year;
-}
-function giveMeMonth(obj){
-	let d = new Date(obj.date);
-	let month = d.getMonth();
-	return month;
-}
-function giveMeDay(obj){
-	let d = new Date(obj.date);
-	let day = d.getDate();
-	return day;
-}
-function giveMeHour(obj){
-	let string = obj.date + " " + obj.time;
-	let d = new Date(string);
-	let hour = d.getHours();
-	return hour;
-}
-function giveMeMinute(obj){
-	let string = obj.date + " " + obj.time;
-	let d = new Date(string);
-	let minute = d.getMinutes();
-	return minute;
-}
-let layer = 1;
-function myFunction(obj){
-	let result;
-	switch(true){
-		case layer == 1:
-		result = giveMeYear(obj);
-		break;
-		case layer == 2:
-		result = giveMeMonth(obj);
-		break;
-		case layer == 3:
-		result = giveMeDay(obj);
-		break;
-		case layer == 4:
-		result = giveMeHour(obj);
-		break;
-		case layer == 5:
-		result = giveMeMinute(obj);
-		break;
-	}
-	return result;
-}
-let itemToCompare;
-let itemToCompareWith;
-function compareItems(){
-	if(myFunction(itemToCompare) < myFunction(itemToCompareWith)){
-		//swap items
-		let rememberItem = itemToCompare;
-		itemToCompare = itemToCompareWith;
-		itemToCompareWith = rememberItem;
-	} if(myFunction(itemToCompare) == myFunction(itemToCompareWith)){
-		//go to different layer
-		layer++;
-		compareItems();
-		layer--;
-	}
-}
-for(let i = 0; i < myArray.length; i++){
-	// console.log(myArray[i]);
-	itemToCompare = myArray[i];
-	for(let moveIndex = 1; moveIndex < myArray.length; moveIndex++){
-		let newIndex;
-		if(i + moveIndex < myArray.length){
-			newIndex = i + moveIndex;
-		} if(i + moveIndex >= myArray.length){
-			newIndex = i + moveIndex - myArray.length;
-		} 
-		itemToCompareWith = myArray[newIndex];
-		compareItems();
-		//write items into array
-		myArray[i] = itemToCompare;
-		myArray[newIndex] = itemToCompareWith;
-	}
-}
-// for(let value of myArray){
-// 	console.log(value.date + " " + value.time);
+// function giveMeMonth(obj){
+// 	let d = new Date(obj.dateAndTime);
+// 	let month = d.getMonth();
+// 	return month;
 // }
-// console.log(myArray)
+// function giveMeDay(obj){
+// 	let d = new Date(obj.dateAndTime);
+// 	let day = d.getDate();
+// 	return day;
+// }
+// function giveMeHour(obj){
+// 	let d = new Date(obj.dateAndTime);
+// 	let hour = d.getHours();
+// 	return hour;
+// }
+// function giveMeMinute(obj){
+// 	let d = new Date(obj.dateAndTime);
+// 	let minute = d.getMinutes();
+// 	return minute;
+// }
+// let layer = 1;
+// function myFunction(obj){
+// 	let result;
+// 	switch(true){
+// 		case layer == 1:
+// 		result = giveMeYear(obj);
+// 		break;
+// 		case layer == 2:
+// 		result = giveMeMonth(obj);
+// 		break;
+// 		case layer == 3:
+// 		result = giveMeDay(obj);
+// 		break;
+// 		case layer == 4:
+// 		result = giveMeHour(obj);
+// 		break;
+// 		case layer == 5:
+// 		result = giveMeMinute(obj);
+// 		break;
+// 	}
+// 	return result;
+// }
+// let itemToCompare;
+// let itemToCompareWith;
+// function compareItems(){
+// 	if(myFunction(itemToCompare) < myFunction(itemToCompareWith)){
+// 		//swap items
+// 		let rememberItem = itemToCompare;
+// 		itemToCompare = itemToCompareWith;
+// 		itemToCompareWith = rememberItem;
+// 	} if(myFunction(itemToCompare) == myFunction(itemToCompareWith)){
+// 		//go to next layer
+// 		layer++;
+// 		compareItems();
+// 		//go back to previous layer
+// 		layer--;
+// 	}
+// }
+// for(let i = 0; i < myArray.length; i++){
+// 	itemToCompare = myArray[i];
+// 	for(let moveIndex = 1; moveIndex < myArray.length; moveIndex++){
+// 		let newIndex;
+// 		if(i + moveIndex < myArray.length){
+// 			newIndex = i + moveIndex;
+// 		} if(i + moveIndex >= myArray.length){
+// 			newIndex = i + moveIndex - myArray.length;
+// 		} 
+// 		itemToCompareWith = myArray[newIndex];
+// 		compareItems();
+// 		//write items into array
+// 		myArray[i] = itemToCompare;
+// 		myArray[newIndex] = itemToCompareWith;
+// 	}
+// }
